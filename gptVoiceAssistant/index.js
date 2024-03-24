fb68a6a7815d35ecfb6033ae6d3f6b8078f4e882
Chirag <chiraggarg.red@gmail.com>
2024-03-24 13:37:04 +0530

    speech to text model added

--- gptVoiceAssistant/index.js
+++ gptVoiceAssistant/index.js
@@ -3,8 +3,6 @@ import express from 'express';
 import path from 'path';
 import 'dotenv/config'
 import { fileURLToPath } from 'url';
-import OpenAI from 'openai';
-
 
 const secretKey = process.env.OPENAI_API_KEY;
 // console.log(secretKey);
@@ -15,14 +13,30 @@ app.use(express.urlencoded({ extended: true }));
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const openai = new OpenAI({
+    apiKey: secretKey,
+})
+
+const messages = [];
+async function main(input) {
+    messages.push({ role: 'user', content: input });
+    console.log(messages);
+    const completion = await openai.chat.completions.create({
+        messages: messages,
+        model: 'gpt-3.5-turbo'
+    });
+    return completion.choices[0]?.message?.content;
+}
+// main("hi")
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.post('/api', function (req, res, next) {
+app.post('/api', async function (req, res, next) {
     console.log(req.body);
-    res.json(req.body);
+    const output = await main(req.body.input);
+    res.json({ success: true, message: output });
 });
 
 app.listen(process.env.PORT, () => {
